131b9b19e2c65c1c03e954e81e1a3b53aabeba3c
備 <1601684622@qq.com>
2024-11-18 16:21:23 +0800

    json_parser.py update

--- 爬虫/自建工具/json_parser.py
+++ 爬虫/自建工具/json_parser.py
@@ -83,7 +83,7 @@ def find_value_in_json(json_data, target_value, current_path=None, result=None):
     return result
 
 
-def get_path_value_dict(json_data, match_str, is_value=False):
+def get_path_value_dict(json_data, match_str, is_value=False, path_filter="", only_values=True):
     if is_value:
         result = find_value_in_json(json_data, match_str)
         result = {match_str: result}
@@ -104,19 +104,37 @@ def get_path_value_dict(json_data, match_str, is_value=False):
             sum_path_dict.update({sum_path: temp_name})
     result_dict = {}
     for sum_path, name in sum_path_dict.items():
-        result_dict.update({sum_path: {name: eval(f"json_data{sum_path}")}})
+        if sum_path.__contains__(path_filter):
+            result_dict.update({sum_path: {name: eval(f"json_data{sum_path}")}})
+    if only_values:
+        return list(result_dict.values())
     return result_dict
+
+
+def show_all(json_data, key, path_filter=""):
+    for k, v in get_path_value_dict(json_data, key, path_filter=path_filter, only_values=False).items():
+        print("key==>", k)
+        print("value==>", v)
 
 
 # with open("ex_api1.json", "r", encoding="utf8") as f:
 #     json_data = json.load(f)
-with open("most_api_1.json", "r", encoding="utf8") as f:
+# with open("most_api_1.json", "r", encoding="utf8") as f:
+#     json_data = json.load(f)
+# with open("index_api1.json", "r", encoding="utf8") as f:
+#     json_data = json.load(f)
+with open("test5.json", "r", encoding="utf8") as f:
     json_data = json.load(f)
 
-target_key = "finalPrice"
-# target_key = 43900
-# for k, v in get_path_value_dict(json_data, target_key, is_value=True).items():
-for k, v in get_path_value_dict(json_data, target_key, is_value=False).items():
-    # print(k, v)
-    print("key=>", k)
-    print("value=>", v)
+# show_all(json_data, "vid", path_filter="")
+# show_all(json_data, "name", path_filter='[0]["values"]')
+# show_all(json_data, "dateRange", path_filter='')
+show_all(json_data, "index", path_filter='')
+# show_all(json_data, "values", path_filter="skuBase")
+names = get_path_value_dict(json_data, "vid")
+values = get_path_value_dict(json_data, "name", path_filter='[0]["values"]')
+middle_obj = {}
+
+for name, value in zip(names, values):
+    middle_obj = name
+    middle_obj.update(value)
